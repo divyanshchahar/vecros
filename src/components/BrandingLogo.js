d9471898681211c5e68505e5eb9a12575adf3f80
divyanshchahar <divyanshchahar@outlook.com>
2023-01-18 16:08:56 +0530

    added branding

---
+++ src/components/BrandingLogo.js
@@ -0,0 +1,11 @@
+import brandingLogo from "../assets/logo_branding.png";
+
+function BrandingLogo() {
+  return (
+    <div className="brnading-logo">
+      <img src={brandingLogo} alt="vecros lgog" />
+    </div>
+  );
+}
+
+export default BrandingLogo;
